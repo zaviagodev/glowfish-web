672126f5da31bb683db33232d83f6f508caa46bd
Nabeel Goraya <itsgoraya@gmail.com>
2025-03-20 11:33:38 +0500

    Fetching the purchased variant to the ticket

--- src/pages/tickets.tsx
+++ src/pages/tickets.tsx
@@ -366,7 +366,7 @@ export default function TicketsPage() {
                       <div>
                         <h3 className="font-medium">{ticket.code}</h3>
                         {/* TODO: fetch the dynamic variant that users have purchased in case the events have variants */}
-                        <p className="text-muted-foreground">varianttest</p>
+                        <p className="text-muted-foreground">{ticket.variant_snapshot?.name}</p>
                       </div>
                     </div>
                     <div
