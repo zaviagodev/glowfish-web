548b8b96c75f3bb514e8ff8e6898994b2cab1d32
Nabeel Goraya <itsgoraya@gmail.com>
2025-03-20 10:04:52 +0500

    marge code from mark

--- src/components/ui/contact-us-button.tsx
+++ src/components/ui/contact-us-button.tsx
@@ -1,9 +1,19 @@
 import { MessageCircleMore } from "lucide-react";
 import { Button } from "./button";
+import { useConfig } from "@/hooks/useConfig";
 
 const ContactUsButton = () => {
+  const { config } = useConfig();
+  const contactUrl = config?.contactLink || "#";
+
+  console.log(config);
+  
+
   return (
-    <Button className="!bg-transparent flex items-center gap-2 text-foreground mx-auto p-0">
+    <Button 
+      className="!bg-transparent flex items-center gap-2 text-foreground mx-auto p-0"
+      onClick={() => window.open(contactUrl, "_blank", "noopener,noreferrer")}
+    >
       <MessageCircleMore className="h-4 w-4" />
       Contact Us
     </Button>
