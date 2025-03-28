bb94d05608f5808381940446161e66e4addea60f
MarkSuttirak <suttirak.ch@zaviago.com>
2025-03-28 10:05:30 +0700

    Change the welcome text

--- src/features/auth/components/ProfileForm.tsx
+++ src/features/auth/components/ProfileForm.tsx
@@ -224,7 +224,7 @@ export const ProfileForm = ({
           </Avatar>
           {isProfileSetup && (
             <h1 className="main-heading">
-              Become a part of Good Afterwork Community and see the latest event
+              Welcome! Please fill in your personal information to continue.
             </h1>
           )}
           <input
