c2cc7ac50a45f65ffb7296604c293987c925aff0
MarkSuttirak <suttirak.ch@zaviago.com>
2025-03-31 16:38:47 +0700

    Change the link to 'tell-us' page to show the welcome drawer

--- src/features/auth/components/ProfileSetup.tsx
+++ src/features/auth/components/ProfileSetup.tsx
@@ -4,7 +4,7 @@ import { ProfileForm } from "./ProfileForm";
 
 export const ProfileSetup = () => {
   const navigate = useNavigate();
-  const returnTo = "/home";
+  const returnTo = "/auth/tell-us-about-yourself";
 
   return (
     <div className="max-width-mobile pt-14">
